6573be83b5ab6cc2d82e61a4bfe7c9f841e4a065
piyushbest <42518831+piyushbest@users.noreply.github.com>
2019-01-25 18:38:07 +0530

    Update JSONDatabase.php

--- src/muqsit/skywars/database/JSONDatabase.php
+++ src/muqsit/skywars/database/JSONDatabase.php
@@ -36,7 +36,7 @@ class JSONDatabase extends Database {
     public function initializeScoreboard() : void
     {
         if (empty($this->scoreboard)) {
-            echo "Initializing JSON scoreboard...";
+           $plugin->getLogger()->echo "Initializing JSON scoreboard...";
             foreach (scandir($this->folder_path) as $file) {
                 if (substr($file, -5) === ".json") {
                     echo "\r\033[K[Reading file " . $file;
